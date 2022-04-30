f231255c47dc114d066b580a86fed441b6ad9782
Gavriel Dabbah <gavriel3@gmail.com>
2022-04-30 07:58:17 +0300

    Update ProjectApplication.java

--- src/main/java/com/flightsystem/project/ProjectApplication.java
+++ src/main/java/com/flightsystem/project/ProjectApplication.java
@@ -1,10 +1,5 @@
 package com.flightsystem.project;
 
-
-import com.flightsystem.project.aspect.SerialNumRepo;
-import com.flightsystem.project.aspect.SerialNumber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
